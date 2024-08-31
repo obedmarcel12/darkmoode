c202f57ac8689da5007b9bc0d64e753ca83fb1f9
obedgodfrey <obedmarcelcomedian@gmail.com>
2024-08-31 08:10:24 +0100

    commit

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function Menu(e) {
   const Figma = document.querySelector('#Figma')
   const value = document.querySelector('#value')
   const Border = document.getElementById('Border')
+  const secondBorder = document.getElementById('secondBorder')
   //
   if (e.name === 'moon') {
-    e.name = 'close'
+    e.name = 'sunny'
     list.classList.add('bg-neutral-800', 'text-[white]')
     Moon.classList.add('text-black')
-    button.classList.add('bg-[#0a4045]', 'text-[white]')
+    button.classList.add('bg-green-800', 'text-[white]')
     npm.classList.add('bg-neutral-700', 'shadow-lg')
     Figma.classList.add(
       'border-[1px]',
@@ -21,12 +22,13 @@ function Menu(e) {
       'hover:shadow-2xl'
     )
     value.classList.add('bg-neutral-700', 'shadow-lg')
-    Border.classList.add('border-[1px]', 'border-[blue]')
+    Border.classList.add('border-[1px]', 'border-neutral-500')
+    secondBorder.classList.add('border-[1px]', 'border-neutral-500')
   } else {
     e.name = 'moon'
     list.classList.remove('bg-neutral-800', 'text-[white]')
     Moon.classList.remove('text-black')
-    button.classList.remove('bg-[#0a4045]', 'text-[white]')
+    button.classList.remove('bg-green-800', 'text-[white]')
     npm.classList.remove('bg-neutral-700', 'shadow-lg')
     Figma.classList.remove(
       'border-[1px]',
@@ -36,6 +38,96 @@ function Menu(e) {
       'hover:shadow-2xl'
     )
     value.classList.remove('bg-neutral-700', 'shadow-lg')
-    Border.classList.remove('border-[1px]', 'border-[blue]')
+    Border.classList.remove('border-[1px]', 'border-neutral-500')
+    secondBorder.classList.remove('border-[1px]', 'border-neutral-500')
+  }
+}
+
+//
+
+function MenuPhone(a) {
+  const click = document.querySelector('#Nav')
+  const bootstrap = document.getElementById('bootstrap')
+  const Menu = document.getElementById('Menu')
+  if (a.name === 'menu') {
+    a.name = 'close'
+    click.classList.add('opacity-[100]', 'bg-[#2a4fcb]', 'z-50')
+    Menu.classList.add(
+      'border-[2px]',
+      'rounded-[50%]',
+      'p-[5px]',
+      'w-[10vw]',
+      'h-[10vw]',
+      'items-center',
+      'justify-center',
+      'flex',
+      'border-[white]'
+    )
+    bootstrap.classList.add('opacity-0')
+  } else {
+    a.name = 'menu'
+    click.classList.remove('opacity-[100]', 'bg-[#2a4fcb]', 'z-50')
+    Menu.classList.remove(
+      'border-[2px]',
+      'rounded-[50%]',
+      'p-[5px]',
+      'w-[10vw]',
+      'h-[10vw]',
+      'items-center',
+      'justify-center',
+      'flex',
+      'border-[white]'
+    )
+    bootstrap.classList.remove('opacity-0')
+  }
+}
+
+function Sunny(event) {
+  const diversed = document.querySelector('#Main-phone')
+  const ButtonPhone = document.querySelector('#ButtonPhone')
+  const npmPhone = document.querySelector('#npmPhone')
+  const FigmaPhone = document.querySelector('#FigmaPhone')
+  const valuePhone = document.querySelector('#valuePhone')
+  const BorderPhone = document.getElementById('BorderPhone')
+  const secondBorderPhone = document.getElementById('secondBorderPhone')
+  const bright = document.querySelector('#bright')
+
+  if (event.name === 'sunny') {
+    event.name = 'moon'
+    bright.classList.add('text-black')
+    diversed.classList.add('bg-neutral-800', 'text-[white]')
+    ButtonPhone.classList.add('bg-green-800', 'text-[white]')
+    npmPhone.classList.add('bg-neutral-700', 'shadow-lg')
+    FigmaPhone.classList.add(
+      'border-[1px]',
+      'border-white',
+      'hover:bg-[white]',
+      'hover:text-black',
+      'hover:shadow-2xl'
+    )
+    valuePhone.classList.add(
+      'bg-neutral-700',
+      'shadow-lg',
+      'bg-neutral-700',
+      'shadow-lg'
+    )
+    BorderPhone.classList.add('border-[1px]', 'border-neutral-500')
+    secondBorderPhone.classList.add('border-[1px]', 'border-neutral-500')
+  } else {
+    event.name = 'sunny'
+    bright.classList.remove('text-black')
+    diversed.classList.remove('bg-neutral-800', 'text-[white]')
+    ButtonPhone.classList.remove('bg-green-800', 'text-[white]')
+    npmPhone.classList.remove('bg-neutral-700', 'shadow-lg')
+    FigmaPhone.classList.remove(
+      'border-[1px]',
+      'border-white',
+      'hover:bg-[white]',
+      'hover:text-black',
+      'hover:shadow-2xl'
+    )
+    valuePhone.classList.remove('bg-neutral-700', 'shadow-lg')
+    BorderPhone.classList.remove('border-[1px]', 'border-neutral-500')
+    secondBorderPhone.classList.remove('border-[1px]', 'border-neutral-500')
   }
 }
